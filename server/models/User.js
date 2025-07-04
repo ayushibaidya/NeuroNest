9708b138e2ccd8c4405afe1b7d62a2c8bdc88f82
Ayushi Baidya <ayushibaidya@Ayushis-MacBook-Air.local>
2025-07-04 21:16:16 +0200

    MongoDB Connected

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'; 
 
-const userSchema = new mongoose.userSchema({
+const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import authRoutes from './routes/authRoutes.js'
-import moodLogRoutes from './routes/moodLogRoutes.js'
-import taskRoutes from './routes/taskRoutes.js'
+// import moodLogRoutes from './routes/moodLogRoutes.js'
+// import taskRoutes from './routes/taskRoutes.js'
 
 dotenv.config()
 
@@ -14,22 +14,21 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
 })); 
+app.use(express.json());
 
 app.use('/api/auth', authRoutes); 
-app.use('/api/mood', moodLogRoutes); 
-app.use('/api/tasks', taskRoutes);
+// app.use('/api/mood', moodLogRoutes); 
+// app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
     res.send("NeuroNest backend running with ESM!");
 }); 
 
 //connect to mongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
     console.log("Connected to MongoDB"); 
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
 }).catch((error) => {
-     console.error("MongoDB connection error:", error);
-}); 
+     console.error("MongoDB connection error:", error.message);
+});
